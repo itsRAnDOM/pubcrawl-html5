a717d7130271782ab2c315b5b9f00b8d874b5202
Brian LeGros <me@brianlegros.com>
2010-09-20 22:10:24 -0400

    fixed fixture overwrite issue for key getter test on pub.

--- tests.js
+++ tests.js
@@ -1,17 +1,17 @@
 module("Pub");
 
-var fixture = new Pub();
+var pubFixture = new Pub();
 
 test("key getter", function() {
-	fixture.point = {lat: 1234, long: 5678};
-	equals(fixture.key, "1234,5678", "Key property did not return comma delimited version of Point");
+	pubFixture.point = {lat: 1234, long: 5678};
+	equals(pubFixture.key, "1234,5678", "Key property did not return comma delimited version of Point");
 });
 
 module("Crawl");
 
 var pub1 = new Pub();
 var pub2 = new Pub();
-fixture = new Crawl();
+var crawlFixture = new Crawl();
 
 test("key getter", function() {
 	ok(false, "FAIL!");
