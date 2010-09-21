355935e8338991561959456ba67b199bf92c446a
Chris Williams <chris.d.williams@gmail.com>
2010-09-21 19:11:02 -0400

    Added passing tests

--- tests.js
+++ tests.js
@@ -10,17 +10,28 @@ test("key getter", function() {
 module("Crawl");
 
 var pub1 = new Pub();
+pub1.name = "Pub 1"
+pub1.point = {lat: 1234, long: 5678};
+
 var pub2 = new Pub();
+pub2.name = "Pub 2"
+pub2.point = {lat: 12345, long: 56789};
+
 var crawlFixture = new Crawl();
 
 test("key getter", function() {
-	ok(false, "FAIL!");
+	crawlFixture.name = "Crawl Name";
+	equals(crawlFixture.key, "Crawl Name", "Key property did not return the name");
 });
 
 test("add pubs with no duplicates", function() {
-	ok(false, "FAIL!");
+	crawlFixture.addPub(pub1);
+	crawlFixture.addPub(pub2);
+	equals(crawlFixture.pubs.length, 2, "Pubs[] is not equal to 2");
 });
 
 test("add pubs with duplicates", function() {
-	ok(false, "FAIL!");
+	crawlFixture.addPub(pub1);
+	crawlFixture.addPub(pub1);
+	equals(crawlFixture.pubs.length, 2, "Pubs[] is not equal to 1");
 });
